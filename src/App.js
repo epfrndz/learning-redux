106b704666906b922389ec0ccdf725982a508730
Edward Fernandez <75320417+epfrndz@users.noreply.github.com>
2022-11-07 22:39:46 -0800

    Integrated redux state and actions to app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment } from './redux/counter';
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const { count } = useSelector(state => state.counter);
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <h1>The count is: {count}</h1>
-      <button onClick={() => {setCount(count + 1)}}>Increment</button>
-      <button onClick={() => {setCount(count - 1)}}>Decrement</button>
+      <button onClick={() => {dispatch(increment())}}>Increment</button>
+      <button onClick={() => {dispatch(decrement())}}>Decrement</button>
     </div>
   );
 }
